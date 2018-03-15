25653733c7d51290e680e74a0f988b38e2d8121c
lihaiyan <lihaiyan@navercorp.com>
2018-03-15 17:24:29 +0800

    add save preprocessed data logic

--- algorithms/nlp/Utils/utilTools.py
+++ algorithms/nlp/Utils/utilTools.py
@@ -2,8 +2,18 @@
 
 # In[ ]:
 import socket
+import pickle
 
 def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
+
+def pickleDump(filename, obj):
+    #store the content
+    with open(filename, 'wb') as handle:
+        pickle.dump(obj, handle)
+        
+def pickleLoad(filename):
+    #load the content
+    return pickle.load(open(filename, "rb" ) )
