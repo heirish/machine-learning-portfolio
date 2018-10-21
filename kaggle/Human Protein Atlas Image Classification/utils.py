63b09170441095af921aa941fe3279a7c766ed44
heirish <heirishlee@gmail.com>
2018-10-21 14:01:28 +0800

    add model

--- kaggle/Human Protein Atlas Image Classification/utils.py
+++ kaggle/Human Protein Atlas Image Classification/utils.py
@@ -2,9 +2,13 @@
 functions in the file are all common utils that other module need
 """
 import pandas as pd
-import numpy as pd
+import numpy as np
 import os
 import shutil
+import matplotlib.pyplot as plt
+from keras.utils.vis_utils import plot_model, model_to_dot #newer
+#from keras.utils.visualize_util import plot, model_to_dot
+from IPython.display import Image, SVG
 
 def readCategories(fileName="./categories.txt"):
     """
@@ -46,3 +50,52 @@ def rebuildDir(dir):
     if os.path.exists(dir):
         shutil.rmtree(dir)
     os.makedirs(dir)
+    
+    
+# visualize model
+def visualizeModel(model, modelName=None):
+    if model == None or modelName == None:
+        raise Exception("in save_model, invalid parameter")    
+    imageName = modelName + ".png"
+    plot_model(model, to_file=imageName, show_shapes=True) #newer
+    #plot(model, to_file=image_name, show_shapes=True)
+    SVG(model_to_dot(model).create(prog='dot', format='svg'))
+    
+def visualizeHistory(history, modelName=None):
+    if modelName == None:
+        raise Exception("in visualize_history, please input your model_name")
+    print(history.history.keys())
+    plt.figure(figsize=(12,4))
+    plt.subplot(1, 2,1)
+    
+    # summarize history for accuracy
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title(modelName + ' model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'valid'], loc='center right')
+    plt.subplots_adjust(wspace = .5)
+    
+    # summarize history for loss
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title(modelName + ' model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'valid'], loc='center right')
+    
+    plt.show()
+    
+#import h5py as h5py
+try:
+    import h5py
+except ImportError:
+    h5py = None
+def saveModel(model, modelName):
+    if model == None or modelName == None:
+        raise Exception("in save_model, invalid parameter")
+    #model.save_weights(modelName + '.h5')
+    with open(modelName + '.json', 'w') as f:
+        f.write(model.to_json())
